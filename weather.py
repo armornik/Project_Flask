034db15dab9591ca7cfb2bb0c95d627e5cb7930f
kmoscow <kd200681@mail.ru>
2020-03-03 23:55:18 +0300

    processing except

--- weather.py
+++ weather.py
@@ -1,17 +1,32 @@
 import requests
-from params_weather import params
+from params_weather import key_id
+
+
 
 def weather_by_city(city_name):
     weather_url = 'https://api.worldweatheronline.com/premium/v1/weather.ashx'
+    params = {
+        'key': key_id,
+        'q': city_name,
+        'format': 'json',
+        'num_of_days': '1',
+        'lang': 'ru'
+    }
 
-    result = requests.get(weather_url, params=params(city_name))
-    weather = result.json()
-    if 'data' in weather:
-        if 'current_condition' in weather['data']:
-            try:
-                return weather['data']['current_condition'][0]
-            except(IndexError, TypeError):
-                return False
+    try:
+        result = requests.get(weather_url, params=params)
+        # Обработка 400 и 500 ошибок
+        result.raise_for_status()
+        weather = result.json()
+        if 'data' in weather:
+            if 'current_condition' in weather['data']:
+                try:
+                    return weather['data']['current_condition'][0]
+                except(IndexError, TypeError):
+                    return False
+    except(requests.RequestException, ValueError):
+        print('Сетевая ошибка')
+        return False
     return False
 
 if __name__ == "__main__":
