f9cb6580f6a6465136104a04b9dfc4df55daca33
kmoscow <kd200681@mail.ru>
2020-03-05 20:23:40 +0300

    Scriping News

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+
+from python_org_news import get_python_news
 from weather import weather_by_city
 
 # initialization our application with name 'server'
@@ -9,7 +11,8 @@ app = Flask(__name__)
 def index():
     title = 'Новости Python'
     weather = weather_by_city('Moscow,Russia')
-    return render_template('index.html', page_title=title, weather=weather)
+    news_list = get_python_news()
+    return render_template('index.html', page_title=title, weather=weather, news_list=news_list)
 
 
 # run our application
