16c422ed56b2739f05ec5d9f64f8e238dd59f9ee
kmoscow <kd200681@mail.ru>
2020-03-13 00:13:29 +0300

    add database Flask-SQLAlchemy

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
-from webapp.model import db
-from webapp.python_org_news import get_python_news
+from webapp.model import db, News
+# from webapp.python_org_news import get_python_news
 from webapp.weather import weather_by_city
 
 
@@ -18,7 +18,10 @@ def create_app():
     def index():
         title = 'Новости Python'
         weather = weather_by_city(app.config['WEATHER_DEFAULT_CITY'])
-        news_list = get_python_news()
+        # news_list = get_python_news()
+        # order_by(News.published) - Сортировка по дате.
+        # desc() - в обратном порядке
+        news_list = News.query.order_by(News.published.desc()).all()
         return render_template('index.html', page_title=title, weather=weather, news_list=news_list)
     return app
 
