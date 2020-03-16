a2606dd4a5c26a17b097848d476549782ce78c38
kmoscow <kd200681@mail.ru>
2020-03-16 20:10:05 +0300

    add page and forms autorization

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 
 from webapp.model import db, News
+from webapp.forms import LoginForm
 # from webapp.python_org_news import get_python_news
 from webapp.weather import weather_by_city
 
@@ -23,5 +24,10 @@ def create_app():
         # desc() - в обратном порядке
         news_list = News.query.order_by(News.published.desc()).all()
         return render_template('index.html', page_title=title, weather=weather, news_list=news_list)
-    return app
 
+    @app.route('/login')
+    def login():
+        title = 'Авторизация'
+        login_form = LoginForm()
+        return render_template('login.html', page_title=title, form=login_form)
+    return app
